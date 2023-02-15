da1890529ead13071ef9391ab69710dfedd88912
Jack David Mullins <jackmullins06@gmail.com>
2023-02-15 20:56:38 +0000

    Implemented a function that randomly generates a startpoint from 23 possible tracks.

--- theTTP.py
+++ theTTP.py
@@ -1,5 +1,6 @@
 import ssl
 import certifi
+import random
 import geopy.geocoders
 from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
@@ -9,6 +10,16 @@ geopy.geocoders.options.default_ssl_context = ctx
 
 geolocator = Nominatim(user_agent="The Travelling Team Principal", timeout=None)
 
+race_tracks = ["Bahrain International Circuit", "Jeddah Corniche Circuit ", "Melbourne", "Baku", "Miami", "Imola", "Monaco",
+               "Barcelona", "Montreal", "Red Bull Ring", "Silverstone", "Budapest", "Stavelot",
+               "Zandvoort", "Monza", "Singapore", "Suzuka", "Doha", "Austin", "Mexico City",
+                "Interlagos", "Las Vegas", "Abu Dhabi"]
+
+def pick_startpoint():    
+    start_point = random.choice(race_tracks)
+    route = [start_point]
+    print("The starting track is {}".format(start_point))
+
 def calculate_distance(starting_track, next_track):
     start_location = round(geolocator.geocode(starting_track).latitude, 5), round(geolocator.geocode(starting_track).longitude, 5)
     next_location = round(geolocator.geocode(next_track).latitude, 5), round(geolocator.geocode(next_track).longitude, 5)
@@ -17,6 +28,4 @@ def calculate_distance(starting_track, next_track):
 
     return round(distance, 2)
 
-
-
-print(calculate_distance("New York", "Los Angeles")) 
+pick_startpoint()
